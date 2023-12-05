fa1f6d786ef96baf44931f2a3a1d986e64fe9f58
Pauline <4224001+paulineribeyre@users.noreply.github.com>
2023-12-05 12:01:13 -0600

    change dir

--- sidecar.sh
+++ sidecar.sh
@@ -108,7 +108,12 @@ function get_access_token() {
 
 function mount_hatchery_files() {
     log "Mounting Hatchery files"
-    cat - > "/data/welcome.html" <<EOM
+    FOLDER="/mounted-files"
+    if [ ! -d "$FOLDER" ]; then
+        mkdir $FOLDER
+    fi
+
+    cat - > "/mounted-files/welcome.html" <<EOM
 <!doctypehtml><html lang=en><title>Nextflow Workspace</title><link href="https://fonts.googleapis.com/icon?family=Source+Sans+Pro"rel=stylesheet><style>body{font-family:"Source Sans Pro",sans-serif;background:#f5f5f5;margin:0;padding:0 20px 20px 20px;font-size:14px;line-height:1.6em;letter-spacing:.02rem}h1.header{margin:20px 10px 16px;border-bottom:solid #421c52 2px;font-size:32px;font-weight:600;line-height:2em;letter-spacing:0;color:#000}.content{padding:10px}</style><h1 class=header>Welcome to the Nextflow Workspace</h1><div class=content><p><strong>This is your personal workspace. The "pd" folder represents your persistent drive:</strong><ul><li>The files you save here will still be available when you come back after terminating your workspace session.<li>Any personal files outside of this folder will be lost.</ul><h2 class=header>Get started with Nextflow</h2><p>If you are new to Nextflow, visit <a href=https://www.nextflow.io>nextflow.io</a> for detailed information.<p>This workspace is set up to run Nextflow workflows in AWS Batch. Your Nextflow configuration must include the Batch queue, IAM role ARN and work directory that were created for you. The configuration below will allow you to run simple workflows and can be adapted to your needs.<p><strong>nextflow.config</strong><pre>
 	plugins {
 		id 'nf-amazon'
@@ -129,8 +134,6 @@ function mount_hatchery_files() {
 	nextflow run hello
 	</pre></div>
 EOM
-    log "ls..."
-    ls
 }
 
 function main() {
