afd6e16205788b1485c73824e22f51058803b23d
Piotr Senkow <piotrsenkow@gmail.com>
2024-08-02 14:20:00 -0500

    formatting

--- sidecar.sh
+++ sidecar.sh
@@ -8,7 +8,6 @@ function log(){
     echo "${TIMESTAMP}: $1" >> ${LOGFILE}
 }
 
-# Shouldn't this be a function?
 populate_notebook() {
     MANIFEST=$1
     shift
@@ -91,33 +90,9 @@ function populate() {
         fi
         done
     }
-
+`
     echo $MANIFESTS | jq -c '.manifests' | process_files manifests
     echo $METADATA | jq -c '.external_file_metadata' | process_files metadata
-
-    # #  Loop over each exported manifest
-    # echo $MANIFESTS | jq -c '.manifests[]' | while read i; do
-    #     FILENAME=$(echo "${i}" | jq -r .filename)
-    #     FOLDERNAME=$(echo "${FILENAME%.*}")
-    #     FOLDER="/data/${GEN3_ENDPOINT}/exported-${FOLDERNAME}"
-    #     # /data/${GEN3_ENDPOINT}/exported-manifests/exported-${FOLDERNAME}
-    #     # /data/${GEN3_ENDPOINT}/exported-metadata/exported-${FOLDERNAME}
-    #     # ^ only need to put in the metadata.json
-    #     if [ ! -d "$FOLDER" ]; then
-    #         log "mkdir -p $FOLDER"
-    #         mkdir -p $FOLDER
-
-    #         # make sure folder can be written to by notebook
-    #         chown -R 1000:100 $FOLDER
-    #         MANIFEST=$(curl -s -H "Authorization: Bearer ${ACCESS_TOKEN}" "https://$GEN3_ENDPOINT/manifests/file/$FILENAME")
-    #         echo "${MANIFEST}" > $FOLDER/manifest.json
-    #         # only need to do this part for metadata ^ not this part v
-
-    #         log "Creating notebook for $FILENAME"
-    #         cp ./template_manifest.json $FOLDER/data.ipynb
-    #         populate_notebook "$MANIFEST" "$FOLDER"
-    #     fi
-    # done
 
     # Make sure notebook user has write access to the folders
     chown -R 1000:100 /data
@@ -140,7 +115,7 @@ function apikeyfile() {
     fi
 }
 
-function get_gen3_access_token() {
+function get_access_token() {
     log "Getting access token using mounted API key from https://$GEN3_ENDPOINT/user/"
     ACCESS_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST "https://$GEN3_ENDPOINT/user/credentials/api/access_token/" -d "{ \"api_key\": \"${API_KEY}\" }" | jq -r .access_token)
     while [ -z "$ACCESS_TOKEN" ]; do
@@ -197,8 +172,7 @@ function main() {
 
     # Gen3SDK should work if $API_KEY is set
     apikeyfile
-    get_gen3_access_token
-    get_manifest_service_access_token
+    get_access_token
 
     mount_hatchery_files
 
@@ -213,7 +187,7 @@ function main() {
         # If the access token expires, fetch a new access token and try again
         if [[ $(echo "$MANIFESTS" | jq -r '.error') = "Please log in." ]]; then
             echo "Session Expired. Trying again with new access token"
-            get_gen3_access_token
+            get_access_token
         else
             # log "Sleeping for 30 seconds before checking for new manifests."
             sleep 30
