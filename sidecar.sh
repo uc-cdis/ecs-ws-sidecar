7e5bfd49e6badbcfc41cccceb895476c52398753
Piotr Senkow <piotrsenkow@gmail.com>
2024-08-08 12:49:11 -0500

    adjusting code per code review comments

--- sidecar.sh
+++ sidecar.sh
@@ -51,13 +51,13 @@ function populate() {
             log $MANIFEST
         fi
         if [ -z "$METADATA_FILE" ]; then
-            log "Unable to get metadata from '$GEN3_ENDPOINT/metadata/'"
+            log "Unable to get metadata from '$GEN3_ENDPOINT/manifests/metadata/'"
             log $METADATA
         fi
         log "sleeping for 15 seconds before trying again.."
         sleep 15
         MANIFEST=$(curl -s -H "Authorization: Bearer ${ACCESS_TOKEN}" "https://$GEN3_ENDPOINT/manifests/")
-        METADATA=$(curl -s -H "Authorization: Bearer ${ACCESS_TOKEN}" "https://$GEN3_ENDPOINT/metadata/")
+        METADATA=$(curl -s -H "Authorization: Bearer ${ACCESS_TOKEN}" "https://$GEN3_ENDPOINT/manifests/metadata/")
     done
     log "successfully retrieved manifests and metadata for user"
 
@@ -66,34 +66,38 @@ function populate() {
         local data=$2
 
         echo $data | jq -c '.[]' | while read i; do
-        FILENAME=$(echo "${i}" | jq -r .filename)
-        FOLDERNAME=$(echo "${FILENAME%.*}")
-        FOLDER="/data/${GEN3_ENDPOINT}/exported-${base_dir}/exported-${FOLDERNAME}"
+            FILENAME=$(echo "${i}" | jq -r .filename)
+            FOLDERNAME=$(echo "${FILENAME%.*}")
+            FOLDER="/data/${GEN3_ENDPOINT}/exported-${base_dir}/exported-${FOLDERNAME}"
 
-        if [ ! -d "$FOLDER" ]; then
-            log "mkdir -p $FOLDER"
-            mkdir -p $FOLDER
+            if [ ! -d "$FOLDER" ]; then
+                log "mkdir -p $FOLDER"
+                mkdir -p $FOLDER
 
-            # make sure folder can be written to by notebook
-            chown -R 1000:100 $FOLDER
+                # make sure folder can be written to by notebook
+                chown -R 1000:100 $FOLDER
 
-            if ["$base_dir" == "manifests"];then
-                MANIFEST_FILE=$(curl -s -H "Authorization: Bearer ${ACCESS_TOKEN}" "https://$GEN3_ENDPOINT/manifests/file/$FILENAME")
-                echo "${MANIFEST_FILE}" > $FOLDER/manifest.json
-                log "Creating notebook for $FILENAME"
-                cp ./template_manifest.json $FOLDER/data.ipynb
-                populate_notebook "$MANIFEST_FILE" "$FOLDER"
-            elif ["$base_dir" == "metadata"];then
-                METADATA_FILE=$(curl -s -H "Authorization: Bearer ${ACCESS_TOKEN}" "https://$GEN3_ENDPOINT/manifests/metadata/$FILENAME")
-                echo "${METADATA_FILE}" > $FOLDER/metadata.json
+                if [["$base_dir" == "manifests"]];then
+                    MANIFEST_FILE=$(curl -s -H "Authorization: Bearer ${ACCESS_TOKEN}" "https://$GEN3_ENDPOINT/manifests/file/$FILENAME")
+                    echo "${MANIFEST_FILE}" > $FOLDER/manifest.json
+                    log "Creating notebook for $FILENAME"
+                    cp ./template_manifest.json $FOLDER/data.ipynb
+                    populate_notebook "$MANIFEST_FILE" "$FOLDER"
+                elif [["$base_dir" == "metadata"]];then
+                    METADATA_FILE=$(curl -s -H "Authorization: Bearer ${ACCESS_TOKEN}" "https://$GEN3_ENDPOINT/manifests/metadata/$FILENAME")
+                    echo "${METADATA_FILE}" > $FOLDER/metadata.json
+                fi
             fi
-        fi
         done
     }
     # echo $MANIFEST_FILE | jq -c '.manifests' | process_files manifests
-    process_files "manifests" "$(echo $MANIFEST_FILE | jq -c '.manifests')"
+    if [ -n "$MANIFEST_FILE" ]; then
+        process_files "manifests" "$(echo $MANIFEST_FILE | jq -c '.manifests')"
+    fi
     # echo $METADATA_FILE | jq -c '.external_file_metadata' | process_files metadata
-    process_files "metadata" "$(echo $METADATA_FILE | jq -c '.external_file_metadata')"
+    if [ -n "$METADATA_FILE" ]; then
+        process_files "metadata" "$(echo $METADATA_FILE | jq -c '.external_file_metadata')"
+    fi
 
     # Make sure notebook user has write access to the folders
     chown -R 1000:100 /data
